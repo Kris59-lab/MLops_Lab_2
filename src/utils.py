66659388a7d94b49d15194a9218102730c47c3d6
Alex.Aworet <alex.aworet1@gmail.com>
2025-10-22 14:06:14 +0200

    added a print statement in the 'divide' function

--- src/utils.py
+++ src/utils.py
@@ -1,13 +1,12 @@
 
-def multiply(a: float, b: float) -> float:
+def multiply(a : float, b : float) -> float:
     return a * b
 
-
-def divide(a: float, b: float) -> float:
+def divide(a : float, b : float) -> float:
     if b == 0:
         raise ValueError("Cannot divide by zero.")
+    print(f"Dividing {a} by {b}")
     return a / b
-
 
 def add(*args):
     ''''Returns the sum of all the arguments provided.'''
