80e1f89a0a8dcfb9831daa366c256ff68a321e81
Kris59-lab <kristale.mayila@aivancity.education>
2025-10-22 12:02:52 +0200

    created tests for multiply and divide functions.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,5 @@
-from src.utils import add, subtract
+from src.utils import add, subtract, multiply, divide
+import pytest
 
 
 def test_add():
@@ -11,3 +12,16 @@ def test_subtract():
     assert subtract(1, 2, 3) == -4
     assert subtract(50, 35, 15) == 0
     assert subtract(7, 2) == 5
+
+
+def test_multiply():
+    assert multiply(-2, -3) == 6
+    assert multiply(-5, 5) == -25
+    assert multiply(1, 0) == 0
+
+
+def test_divide():
+    assert divide(8, 2) == 4
+    assert divide(-100, 5) == -20
+    with pytest.raises(ValueError, match="Cannot divide by zero."):
+        divide(10, 0)
